1352db73593c15a927a018386ea99203ce9b6723
Yavuz Çelik <63187562+y4wwy@users.noreply.github.com>
2023-06-10 20:14:30 +0300

    ...

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="h-container">
+        <img src="../public/logo.png" alt="Logo" width={100} />
+        <div className="h-menu">
+          <a href="">Residencies</a>
+          <a href="">Our Value</a>
+          <a href="">Contact Us</a>
+          <a href="">Get Started</a>
+          <button className="button">
+            <a href="">Contact</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
