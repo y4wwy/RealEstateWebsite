ef7c82b5d98d38b4fd9aeaa48c36fe81a03d484e
Yavuz Çelik <63187562+y4wwy@users.noreply.github.com>
2023-06-11 01:54:52 +0300

    ...

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
-        <img src="../public/logo.png" alt="Logo" width={100} />
+        <img src="/logo.png" alt="Logo" width={100} />
         <div className="flexCenter h-menu">
           <a href="">Residencies</a>
           <a href="">Our Value</a>
